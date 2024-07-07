eb31f8643528014e0c5dee78a850e33f1db00447
Felipe - PCFELIPE_wsl <felipelullio@gmail.com>
2024-07-07 19:25:14 -0300

    add comments on codes

--- retrieve-data-from-google-sheet.py
+++ retrieve-data-from-google-sheet.py
@@ -14,34 +14,63 @@ import re
 import time
 import threading
 
-# Configuração do serviço do WebDriver
+# Configuração do serviço do WebDriver, driver google chrome
 def setup_driver():
     service = Service(ChromeDriverManager().install())
+    # Configuração do navegador Chrome
     options = webdriver.ChromeOptions()
+    
+    # Abrir navegador maximizado
     options.add_argument("--start-maximized")
+    # Diretório do perfil padrão
     options.add_argument("--user-data-dir=C:/Users/Felipe/AppData/Local/Google/Chrome/User Data")
     options.add_argument("--profile-directory=Default")
     options.add_argument("--disable-blink-features=AutomationControlled")
     options.add_experimental_option("excludeSwitches", ["enable-automation"])
     options.add_experimental_option("useAutomationExtension", False)
+    #options.add_argument("--remote-allow-origins=*")  # Permitir todas as origens remotas
+    #options.add_argument("--window-size=1920,1080")
+    #options.add_argument("--headless") # executar chrome sem interface gráfica
+    #options.add_argument("--disable-extensions")
+    #options.add_argument("--disable-notifications")
+    #options.add_argument("--disable-gpu")
+    #options.add_argument("--ignore-certificate-errors")
+    #options.add_argument("--disable-popup-blocking")  # Desabilita o bloqueio de pop-ups
+    #options.add_argument("--disable-infobars")  # Desabilita as infobars
+    #options.add_argument("--no-first-run")  # Não executa a primeira execução
+    #options.add_argument("--no-sandbox")  # Nenhum ambiente de laboratório
+    #options.add argument ("--remote-debugging-port=0")
+    
+    # Inicialização do driver do Chrome
     driver = webdriver.Chrome(service=service, options=options)
     driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+    
+    # Encerrar o navegador
+    #driver.quit()
     return driver
 
 def get_data_from_google_sheet(url_data, driver):
     try:
+        # Realiza uma requisição GET para obter o conteúdo da planilha
         response = requests.get(url_data)
         response.raise_for_status()  # Verifica se a requisição foi bem-sucedida
+        # OU
+         # Verifica se a requisição foi bem-sucedida (código 200)
+        #if response.status_code == 200:
     except requests.RequestException as e:
         print(f"Erro ao obter dados da planilha: {e}")
         return None
 
+    # Lê o conteúdo CSV retornado pela API do Google Sheets
     csv_data = response.text
+    # Utiliza o módulo csv para ler os dados CSV
     csv_reader = csv.reader(csv_data.splitlines())
 
+    # Criação da janela principal
     root = tk.Tk()
     root.title("Gerenciador de Cursos - Felipe")
 
+     # Criando uma ListView com scroll vertical
     tree = ttk.Treeview(root, columns=("col1", "col2", "col3", "col4", "col5", "col6", "col7"), show="headings")
     tree.heading("col1", text="Index")
     tree.heading("col2", text="Cursos")
@@ -51,29 +80,39 @@ def get_data_from_google_sheet(url_data, driver):
     tree.heading("col6", text="URL Notion")
     tree.heading("col7", text="Coluna 7")
     tree.pack(fill="both", expand=True)
-
+    
+    # Lista para armazenar todas as linhas do CSV
     all_rows = []
+    # Conjunto para armazenar valores únicos da coluna 4 (categorias dos cursos)
     unique_col4_values = set()
 
+    # Itera sobre as linhas do CSV
     for row in csv_reader:
+         # Inserir cada linha na ListView
         tree.insert("", "end", values=row)
+        # salvar linhas do csv na var/lista all_rows 
         all_rows.append(row)
+        # salvar os valores da coluna 4 na lista unique_col4_values
         unique_col4_values.add(row[3])
-
+        
+    # Ajustando a largura das colunas
     for col in tree["columns"]:
+        # Define largura mínima e colocar o conteúdo na esquerda(="w") ou centraliza conteúdo(CENTER)
         tree.column(col, width=1, minwidth=1, anchor="w")
+        # Centraliza o texto do cabeçalho
         tree.heading(col, anchor="w")
     tree.column(1, width=400, minwidth=400, anchor="w")
 
     frame = tk.Frame(root)
     frame.pack(padx=5, pady=10)
 
+    # Dropdown para filtrar por valores da coluna 4
     col4_filter_label = tk.Label(frame, text="Filtrar por Categoria:")
     col4_filter_label.grid(row=0, column=0, padx=(0, 10), sticky="w", pady=10)
 
     col4_values = sorted(list(unique_col4_values))
     col4_filter_var = tk.StringVar(root)
-    col4_filter_var.set("Todos")
+    col4_filter_var.set("Todos") # Define o valor padrão como "Todos"
     col4_filter_dropdown = ttk.Combobox(frame, textvariable=col4_filter_var, values=["Todos"] + col4_values, state="readonly", width=27)
     col4_filter_dropdown.grid(row=0, column=1, sticky="w")
 
@@ -84,41 +123,50 @@ def get_data_from_google_sheet(url_data, driver):
     search_entry = tk.Entry(frame, textvariable=search_var, width=30)
     search_entry.grid(row=1, column=1, sticky="we", padx=(0, 10))
 
+    # Função para filtrar os dados da Treeview com base no valor selecionado no dropdown
     def filter_data_with_combobox(event=None):
         selected_value = col4_filter_var.get()
         tree.delete(*tree.get_children())
+        # Mostra todas as linhas se "Todos" estiver selecionado
         if selected_value == "Todos":
             for row in all_rows:
                 tree.insert("", "end", values=row)
         else:
+            # Filtra as linhas com base no valor selecionado na coluna 4
             filtered_rows = [row for row in all_rows if row[3] == selected_value]
             for row in filtered_rows:
                 tree.insert("", "end", values=row)
-
+    # Liga o evento de seleção do combobox
     col4_filter_dropdown.bind("<<ComboboxSelected>>", filter_data_with_combobox)
 
     def filter_data_with_search(event=None):
         query = search_var.get()
-        tree.delete(*tree.get_children())
+        tree.delete(*tree.get_children()) # Limpa a Treeview antes de aplicar o filtro
         try:
-            regex = re.compile(query, re.IGNORECASE)
+            regex = re.compile(query, re.IGNORECASE) # Regex case insensitive
             for row in all_rows:
                 if any(regex.search(str(value)) for value in row):
                     tree.insert("", "end", values=row)
         except re.error:
             pass
 
+    # Liga o evento de seleção do combobox
     search_var.trace_add("write", lambda *args: filter_data_with_search())
 
+    # Função para lidar com o clique duplo na ListView
     def on_item_click(event):
         item = tree.selection()[0]
         if item:
+            # Obtém os valores da linha clicada
             values = tree.item(item, "values")
-            driver.get(values[2])
-            webbrowser.open(values[5])
-
+            # Abre o navegador com o link da coluna 3 (índice 2)
+            driver.get(values[2]) # Coluna 3 = url do curso usando selenium pois quero abrir no meu perfil Felipe do Chrome
+            # Abrir Notion
+            webbrowser.open(values[5]) # Coluna 6 = url do notion
+    # Define o evento de duplo clique na ListView
     tree.bind("<Double-1>", on_item_click)
-
+    
+    # Iniciar o loop principal da GUI
     root.mainloop()
 
 def pause_and_play_video(driver):
@@ -261,6 +309,7 @@ def toggle_video_subtitles(driver):
     '''
     driver.execute_script(js_toggle_subtitles)
 
+# Função para associar as hotkeys
 def start_keyboard_shortcuts(driver):
     keyboard.add_hotkey('shift+l', lambda: pause_and_play_video(driver))
     keyboard.add_hotkey('shift+x', lambda: teste(driver))
@@ -276,6 +325,7 @@ def start_keyboard_shortcuts(driver):
 
 def main():
     driver = setup_driver()
+    # URL da planilha do Google Sheets
     url_data = "https://docs.google.com/spreadsheets/d/1Fg4cP6VEjQ5Ke8LCTSo88dUdZlc1az2RpBC6Bu6YuSw/gviz/tq?tqx=out:csv&range=A2:G80&sheet=Cursos"
 
     # Inicie a GUI do Tkinter em uma thread separada
